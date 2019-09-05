28eee79994cf3bfdccc434a03ca15d44faceacc2
MalenaLucero <luceromalena@hotmail.com>
2019-09-04 21:02:10 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const server = express()
+const router = require('./modules/router')
+const port = 3000
+
+server.use('/', router)
+server.use(express.static('public'))
+
+server.listen(port, () =>{
+    console.log(`Running on port ${port}`)
+})
