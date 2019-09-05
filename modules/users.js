e53e37c42f6a107ea5cb9b2123e60d0e5e685041
MalenaLucero <luceromalena@hotmail.com>
2019-09-04 21:55:45 -0300

    users added:

---
+++ modules/users.js
@@ -0,0 +1,13 @@
+const users = [
+    {name: 'Daichi Sawamura', email: 'dsawamura@karasuno.com', adress: 'Miyagi 1234', phone: 31181768},
+    {name: 'Asahi Azumane', email: 'aazumane@karasuno.com', adress: 'Spiker 1234', phone: 10181847},
+    {name: 'Yuu Nishinoya', email: 'ynishinoya@karasuno.com', adress: 'Libero 1234', phone: 10171593},
+    {name: 'Kei Tsukishima', email: 'ktsukishima@karasuno.com', adress: 'Spiker 1234', phone: 27161883},
+    {name: 'Tetsurou Kurou', email: 'tkurou@nekoma.com', adress: 'Captain 1234', phone: 17181877},
+    {name: 'Morisuke Yaku', email: 'myaku@nekoma.com', adress: 'Libero 3456', phone:  18081652},
+    {name: 'Touru Oikawa', email: 'toikawa@aobajosai.com', adress: 'Captain 6789', phone: 20181843},
+    {name: 'Hajime Iwaizumi', email: 'hiwaizumi@aobajosai.com', adress: 'Spiker 6778', phone: 10181793},
+    {name: 'Koutarou Bokuto', email: 'kbokuto@fukurodani.com', adress: 'Spiker 0987', phone: 20181853},
+    {name: 'Keiji Akaashi', email: 'kakaashi@fukurodani.com', adress: 'Setter 9876', phone: 50171823},
+    {name: 'Yuuji Terushima', email: 'yterushima@jozenji.com', adress: 'Spiker 7654', phone: 17181772},
+]
