49d4f6cee1a7f60169c5b198abe42c6167ca6678
MalenaLucero <luceromalena@hotmail.com>
2019-09-16 14:38:53 -0300

    no anda

--- modules/router.js
+++ modules/router.js
@@ -16,4 +16,9 @@ router.post('/api/employees', employees.postEmployee)
 router.patch('/api/employees', employees.patchEmployee)
 router.delete('/api/employees/:id', employees.deleteEmployee)
 
+//not found
+/*router.use((req, res) => {
+	res.status(404).send('error')
+})*/
+
 module.exports = router
