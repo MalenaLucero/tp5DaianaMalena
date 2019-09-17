a44b9e2edac72354c356b68e37560f7889697e90
dfigalo1 <daianafigalo@gmail.com>
2019-09-16 21:31:40 -0300

    modal

--- public/main.js
+++ public/main.js
@@ -6,14 +6,12 @@ const initialize = () =>{
 
     // modal
     let modal = document.getElementById('miModal');
-    let flex = document.getElementById('flex');
     let abrir = document.getElementById('abrir');
     let cerrar = document.getElementById('close');
 
     abrir.addEventListener('click',function(){
         modal.style.display = 'block';
     });
-
     cerrar.addEventListener('click',function(){
         modal.style.display = 'none';
     });
